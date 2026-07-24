f8a6c7eb1061399828a7f17536a9af550201caad
Aisha-900 <yasa95vaaaa@gmail.com>
2026-07-25 00:32:15 +0300

    выполнено дз 6

---
+++ main.js
@@ -0,0 +1,48 @@
+//Покраска всех карточек
+
+const productCards = document.querySelectorAll('.product-card');
+const chengeColorAllCardButton = document.querySelector ('#change-color-all-card');
+const greenColorHash ='#0afe3f';
+const blueColorHash = '#f415c4';
+
+
+chengeColorAllCardButton.addEventListener('click', () => {
+    productCards.forEach((card) => card.style.backgroundColor = greenColorHash)
+})
+
+//Покраска первой карточки
+const firstProductCard = document.querySelector('.product-card');
+const chengeColorFirstCardButton = document.querySelector ('#change-color-first-card');
+
+
+chengeColorFirstCardButton.addEventListener('click', () => {
+    productCards.forEach((car) => car.style.backgroundColor = blueColorHash)
+   
+})
+
+//Открыть страницу Google
+
+const openGoogleButton = document.querySelector('#open-gogle');
+openGoogleButton.addEventListener('click', openGoogle)
+
+
+function openGoogle () {
+    const answer = confirm ('Вы действительно хотите открыть Google')
+   if (answer === true) {
+   window.open('https://google.com')
+    } else {
+    return;
+   }
+}
+
+//Вывод консоль лог
+
+const autputLogButton = document.querySelector('#autput-console-log');
+
+autputLogButton.addEventListener('click', () => autputConsoleLog ('дз №4'))
+
+function autputConsoleLog (message) {
+    alert('10')
+    console.log(message)
+}
+
